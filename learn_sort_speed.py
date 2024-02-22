9b0c869646e7f5ef421410a5ca0d9363702ce0cd
mjbundschuh <michael_bundschuh@yahoo.com>
2024-02-21 17:52:30 -0900

    Added dict speed test. Refactored python.

--- learn_sort_speed.py
+++ learn_sort_speed.py
@@ -2,45 +2,45 @@ import time
 
 class myMain:
     def __init__(self):
-        self.nameset = set()
-        self.namelist = list()
-        self.goodlist = ('michael', 'mike', 'paul', 'moni', 'julie', 'greg', 'hannah', 'hanna', 'leroy', 'calli', 'eddie')
-        self.badlist = ('michaelx', 'mikex', 'paulx', 'monix', 'juliex', 'gregx', 'hannahx', 'hannax', 'leroyx', 'callix', 'eddiex')
+        self.name_set = set()
+        self.name_list = list()
+        self.name_dict = dict()
+        self.good_list = ('michael', 'mike', 'paul', 'moni', 'julie', 'greg', 'hannah', 'hanna', 'leroy', 'calli', 'eddie')
+        self.bad_list = ('michaelx', 'mikex', 'paulx', 'monix', 'juliex', 'gregx', 'hannahx', 'hannax', 'leroyx', 'callix', 'eddiex')
         with open('names.txt', 'r') as db:
             for line in db.readlines():
-                self.nameset.add(line.lower().rstrip()) 
-                self.namelist.append(line.lower().rstrip()) 
+                self.name_set.add(line.lower().rstrip()) 
+                self.name_list.append(line.lower().rstrip())
+                self.name_dict[line.lower().rstrip()] = 1
 
     def bsearch(self, val):
         # binary search of values list
         first = 0
-        last = len(self.namelist)-1
+        last = len(self.name_list)-1
         index = -1
         while (first <= last) and (index == -1):
             mid = (first+last)//2
-            if self.namelist[mid] == val:
+            if self.name_list[mid] == val:
                 index = mid
             else:
-                if val<self.namelist[mid]:
+                if val<self.name_list[mid]:
                     last = mid -1
                 else:
                     first = mid +1
         return(index)
     
     def run_test(self, search_type, good_bad, endrange):
-        list = self.goodlist if good_bad == "good" else self.badlist
+        list = self.good_list if good_bad == "good" else self.bad_list
         if good_bad == "good":
             print(f"\n{search_type} search speed: average {endrange}x")
         start = time.process_time()
         for t1 in range(1,endrange):
             for n1 in list:
                 match search_type:
-                    case "set":
-                        isFound = True if n1 in self.nameset else False
-                    case "list":
-                        isFound = True if n1 in self.namelist else False
-                    case "bsearch":
-                        isFound = True if self.bsearch(n1) > -1 else False
+                    case "bsearch": isFound = True if self.bsearch(n1) > -1 else False
+                    case    "dict": isFound = True if n1 in self.name_dict else False
+                    case    "list": isFound = True if n1 in self.name_list else False
+                    case     "set": isFound = True if n1 in self.name_set else False
         end = time.process_time()
         msec = round(((end-start)*1000)/endrange,4)
         print(f"    {good_bad} list: " + str(msec))
@@ -52,13 +52,17 @@ class myMain:
         self.run_test("set", "good", endrange)
         self.run_test("set", " bad", endrange)
 
-        # test list speed
-        self.run_test("list", "good", endrange)
-        self.run_test("list", " bad", endrange)
+        # test dict sort speed
+        self.run_test("dict", "good", endrange)
+        self.run_test("dict", " bad", endrange)
 
         # test binary sort speed
         self.run_test("bsearch", "good", endrange)
         self.run_test("bsearch", " bad", endrange)
+
+        # test list speed
+        self.run_test("list", "good", endrange)
+        self.run_test("list", " bad", endrange)
 
         print()
 
